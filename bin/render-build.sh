3f2689bd0c4413e0b872816284e9cc0859807873
MOHAMMED FAYAZ ISMAIL P K <fayazpk18@gmail.com>
2025-02-03 22:08:18 +0530

    render altered

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,14 +8,14 @@ if ! which ruby; then
   exit 1
 fi
 
+# Ensure correct Ruby path
+export PATH="/opt/render/project/rubies/ruby-3.3.5/bin:$PATH"
+
 # Install dependencies with npm or yarn as fallback
 if ! npm install -g webpack webpack-cli; then
   echo "npm installation failed, trying yarn..."
   yarn add -D webpack webpack-cli || { echo "Failed to install webpack and webpack-cli"; exit 1; }
 fi
-
-# Ensure correct Ruby path
-export PATH="/opt/render/project/rubies/ruby-3.3.5/bin:$PATH"
 
 # Fix permissions
 chmod +x ./bin/rails
